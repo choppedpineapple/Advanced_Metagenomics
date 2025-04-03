403a671846825c24fbb659d1828487721287e22a
sliced_apple <95742135+choppedpineapple@users.noreply.github.com>
2025-04-03 11:04:04 +0100

    Update sfile_fx.sh

--- sfile_fx.sh
+++ sfile_fx.sh
@@ -7,3 +7,16 @@ failed.sort()
 
 # Update remaining strings for next iteration
 remain_strings = failed.copy()
+
+
+
+
+-------
+
+# In the main section of your script, change this line:
+file_list = [os.path.join(input_dir, f) for f in os.listdir(input_dir) if f.endswith('.txt')]
+
+# To this:
+file_list = sorted([os.path.join(input_dir, f) for f in os.listdir(input_dir) if f.endswith('.txt')])
+
+
